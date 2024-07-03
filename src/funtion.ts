c4a8579101445c2d6225605ca2eeb8c6065eb342
SATYAADIL <adilfaishal11@gmail.com>
2024-07-03 21:08:54 +0700

    tugas3 plugin

--- src/funtion.ts
+++ src/funtion.ts
@@ -1,27 +1,40 @@
-function sapa(nama:string){
-    console.log("Halo, aku" + nama);   
+// function sapa(nama:string){
+//     console.log("Halo, aku" + nama);   
+// }
+// sapa("Yasir!")
+
+// function calculate (a:number, b:number, c:number):number{
+//     return a + b  + c
+// }
+// let hasil : number = calculate(3, 5, 1)
+// console.log("Hasil Penjumlahan: " + hasil);
+// let multiplication = (x:number, y:number) => {
+//     return x * y
+// }
+// let hasil2 = multiplication(5,8)
+// console.log("Hasil perkalian: "+ hasil2);
+
+// let cube = function(num:number){
+//     return num **3
+// }   
+// console.log(cube (1));
+
+// let ml : string = "clint"
+// console.log(ml);
+
+function gret (name:string){
+ 
+    console.log("halo aku " + name);
 }
-sapa("Yasir!")
 
-function calculate (a:number, b:number, c:number):number{
-    return a + b  + c
+gret("Satya!")
+
+
+
+let multiply =  (a:number, b:number)=> {
+    return a * b
 }
-let hasil : number = calculate(3, 5, 1)
-console.log("Hasil Penjumlahan: " + hasil);
-let multiplication = (x:number, y:number) => {
-    return x * y
-}
-let hasil2 = multiplication(5,8)
-console.log("Hasil perkalian: "+ hasil2);
-
-let cube = function(num:number){
-    return num **3
-}   
-console.log(cube (1));
-
-let ml : string = "clint"
-console.log(ml);
-
-
+let hasil4 = multiply(3,2)
+console.log("hasil perkalian " + hasil4);
 
 
